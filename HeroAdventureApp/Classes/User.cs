e674e92377d782ea90c0d5351e195e6eda934da6
Marcus Hertzman <marcus.hertzman@gmail.com>
2025-10-14 11:37:51 +0200

    Fixed some null issues

--- HeroAdventureApp/Classes/User.cs
+++ HeroAdventureApp/Classes/User.cs
@@ -6,9 +6,9 @@ namespace HeroAdventureApp.Classes
 {
     public class User
     {
-        public string Username { get; set; }
-        public string Password { get; set; } // Hasha i produktion!
-        public string Email { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; } // Hasha i produktion!
+        public string? Email { get; set; }
         public List<Quest> Quests { get; set; } = new List<Quest>();
         public List<string> Badges { get; set; } = new List<string>(); // T.ex. "Bronze Hero", "Silver Hero"
     }
