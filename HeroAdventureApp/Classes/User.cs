0f02aae24f386495c63ac98bbf64e42fea5f3cc7
Marcus Hertzman <marcus.hertzman@gmail.com>
2025-10-19 12:25:58 +0200

    New User

--- HeroAdventureApp/Classes/User.cs
+++ HeroAdventureApp/Classes/User.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HeroAdventureApp.Classes;
+using Newtonsoft.Json;
+using System;
 
-
-namespace HeroAdventureApp.Classes
+public class User
 {
-    public class User
-    {
-        public string? Username { get; set; }
-        public string? Password { get; set; } // Hasha i produktion!
-        public string? Email { get; set; }
-        public List<Quest> Quests { get; set; } = new List<Quest>();
-        public List<string> Badges { get; set; } = new List<string>(); // T.ex. "Bronze Hero", "Silver Hero"
-    }
+    public string Username { get; set; }
+    public string Password { get; set; } // Hasha i produktion!
+    public string Email { get; set; }
+
+    [JsonProperty("Quests")]
+    public List<Quest> Quests { get; set; } = new List<Quest>();
+
+    [JsonProperty("Badges")]
+    public List<string> Badges { get; set; } = new List<string>();
+
+    // För JSON serialisering av DateTime
+    [JsonProperty(Order = -2)]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
+
